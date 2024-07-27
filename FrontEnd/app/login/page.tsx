cd3173ad9d31c6e6c99f15438d5b4cec7d5f6b5c
Le Quoc Uy <lequocuyit@gmail.com>
2024-07-28 00:04:28 +0700

    add FE

---
+++ FrontEnd/app/login/page.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex justify-center items-start md:items-center p-8">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            This demo uses GitHub for authentication.
+          </CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <form
+            
+            className="w-full"
+          >
+            <Button className="w-full">Sign in with GitHub</Button>
+          </form>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
